751ebeaf9a9b1d152a2c0549fb73e91b38d413ab
Adrian Litewski <litewskidev@gmail.com>
2023-04-21 01:13:26 +0200

    remove thisApp

--- src/js/app.js
+++ src/js/app.js
@@ -5,46 +5,42 @@ import { classNames, select } from './settings.js';
 const app = {
 
   initPages: function(){
-    const thisApp = this;
-
-    thisApp.pages = document.querySelector(select.containerOf.pages).children;
-    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    this.pages = document.querySelector(select.containerOf.pages).children;
+    this.navLinks = document.querySelectorAll(select.nav.links);
 
     const idFromHash = window.location.hash.replace('#', '');
-    let pageMatchingHash = thisApp.pages[0].id;
+    let pageMatchingHash = this.pages[0].id;
 
-    for(let page of thisApp.pages){
-      if(page.id === idFromHash){
+    for (let page of this.pages){
+      if (page.id === idFromHash){
         pageMatchingHash = page.id;
         break;
       }
     }
 
-    thisApp.activatePage(pageMatchingHash);
-
-    for(let link of thisApp.navLinks){
+    this.activatePage(pageMatchingHash);
+    
+    for (let link of this.navLinks){
       link.addEventListener('click', function(event){
         const clickedElement = this;
         event.preventDefault();
         const id = clickedElement.getAttribute('href').replace('#', '');
 
-        thisApp.activatePage(id);
+        this.activatePage(id);
         window.location.hash = '#/' + id;
       });
     }
   },
 
   activatePage: function(pageId){
-    const thisApp = this;
-
-    for(let page of thisApp.pages){
+    for (let page of this.pages){
       page.classList.toggle(
         classNames.pages.active,
         page.id === pageId
       );
     }
 
-    for(let link of thisApp.navLinks){
+    for (let link of this.navLinks){
       link.classList.toggle(
         classNames.nav.active,
         link.getAttribute('href') == '#' + pageId
@@ -53,23 +49,19 @@ const app = {
   },
 
   initAbout: function(){
-    const thisApp = this;
     const aboutContainer = document.querySelector(select.containerOf.about);
-    thisApp.about = new About(aboutContainer);
+    this.about = new About(aboutContainer);
   },
 
   initFinder: function(){
-    const thisApp = this;
     const finderContainer = document.querySelector(select.containerOf.finder);
-    thisApp.finder = new Finder(finderContainer);
+    this.finder = new Finder(finderContainer);
   },
 
   init: function(){
-    const thisApp = this;
-
-    thisApp.initPages();
-    thisApp.initAbout();
-    thisApp.initFinder();
+    this.initPages();
+    this.initAbout();
+    this.initFinder();
   }
 };
 
