bd691084cb5c4074930c6ed052e4a97e793937c4
Adrian Litewski <litewskidev@gmail.com>
2023-04-21 01:24:45 +0200

    restore thisSomething

--- src/js/app.js
+++ src/js/app.js
@@ -5,42 +5,46 @@ import { classNames, select } from './settings.js';
 const app = {
 
   initPages: function(){
-    this.pages = document.querySelector(select.containerOf.pages).children;
-    this.navLinks = document.querySelectorAll(select.nav.links);
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
     const idFromHash = window.location.hash.replace('#', '');
-    let pageMatchingHash = this.pages[0].id;
+    let pageMatchingHash = thisApp.pages[0].id;
 
-    for (let page of this.pages){
+    for (let page of thisApp.pages){
       if (page.id === idFromHash){
         pageMatchingHash = page.id;
         break;
       }
     }
 
-    this.activatePage(pageMatchingHash);
-    
-    for (let link of this.navLinks){
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks){
       link.addEventListener('click', function(event){
         const clickedElement = this;
         event.preventDefault();
         const id = clickedElement.getAttribute('href').replace('#', '');
 
-        this.activatePage(id);
+        thisApp.activatePage(id);
         window.location.hash = '#/' + id;
       });
     }
   },
 
   activatePage: function(pageId){
-    for (let page of this.pages){
+    const thisApp = this;
+
+    for (let page of thisApp.pages){
       page.classList.toggle(
         classNames.pages.active,
         page.id === pageId
       );
     }
 
-    for (let link of this.navLinks){
+    for (let link of thisApp.navLinks){
       link.classList.toggle(
         classNames.nav.active,
         link.getAttribute('href') == '#' + pageId
@@ -49,19 +53,25 @@ const app = {
   },
 
   initAbout: function(){
+    const thisApp = this;
+
     const aboutContainer = document.querySelector(select.containerOf.about);
-    this.about = new About(aboutContainer);
+    thisApp.about = new About(aboutContainer);
   },
 
   initFinder: function(){
+    const thisApp = this;
+
     const finderContainer = document.querySelector(select.containerOf.finder);
-    this.finder = new Finder(finderContainer);
+    thisApp.finder = new Finder(finderContainer);
   },
 
   init: function(){
-    this.initPages();
-    this.initAbout();
-    this.initFinder();
+    const thisApp = this;
+
+    thisApp.initPages();
+    thisApp.initAbout();
+    thisApp.initFinder();
   }
 };
 
--- src/js/components/Finder.js
+++ src/js/components/Finder.js
@@ -4,122 +4,132 @@ import Spot from './Spot.js';
 
 class Finder {
   constructor(element){
-    this.element = element;
+    const thisFinder = this;
+
+    thisFinder.element = element;
 
     //  GRID ------------------------------------
-    this.cols = 10;
-    this.rows = this.cols;
+    thisFinder.cols = 10;
+    thisFinder.rows = thisFinder.cols;
     //  1D ARRAY
-    this.grid = new Array(this.cols);
+    thisFinder.grid = new Array(thisFinder.cols);
     //  2D ARRAY
-    for (let i = 0; i < this.cols; i++){
-      this.grid[i] = new Array(this.rows);
+    for (let i = 0; i < thisFinder.cols; i++){
+      thisFinder.grid[i] = new Array(thisFinder.rows);
     }
     //  SPOTS
-    for (let i = 0; i < this.cols; i++){
-      for (let j = 0; j < this.rows; j++){
-        this.grid[i][j] = new Spot(i, j);
+    for (let i = 0; i < thisFinder.cols; i++){
+      for (let j = 0; j < thisFinder.rows; j++){
+        thisFinder.grid[i][j] = new Spot(i, j);
       }
     } // ----------------------------------------
 
     //  GLOBALS
-    this.clicked = [];
-    this.edgeFields = [];
-    this.path = [];
-    this.openSet = [];
-    this.closedSet = [];
-    this.counter = 0;
-    this.start;
-    this.end;
-    this.gridValues = Object.values(this.grid).map(col => Object.values(col)).flat();
+    thisFinder.clicked = [];
+    thisFinder.edgeFields = [];
+    thisFinder.path = [];
+    thisFinder.openSet = [];
+    thisFinder.closedSet = [];
+    thisFinder.counter = 0;
+    thisFinder.start;
+    thisFinder.end;
+    thisFinder.gridValues = Object.values(thisFinder.grid).map(col => Object.values(col)).flat();
 
     //  INIT
-    this.step = 1;
-    this.render();
+    thisFinder.step = 1;
+    thisFinder.render();
   }
 
   changeStep(newStep){
-    this.step = newStep;
-    this.render();
+    const thisFinder = this;
+
+    thisFinder.step = newStep;
+    thisFinder.render();
   }
 
   setupGrid(){
+    const thisFinder = this;
+
     //  NEIGHBORS
-    for (let i = 0; i < this.cols; i++){
-      for (let j = 0; j < this.rows; j++){
-        this.grid[i][j].addNeighbors(this.grid);
+    for (let i = 0; i < thisFinder.cols; i++){
+      for (let j = 0; j < thisFinder.rows; j++){
+        thisFinder.grid[i][j].addNeighbors(thisFinder.grid);
       }
     }
     //  SHOW GRID
-    for (let i = 0; i < this.cols; i++){
-      for (let j = 0; j < this.rows; j++){
-        this.grid[i][j].showGrid();
+    for (let i = 0; i < thisFinder.cols; i++){
+      for (let j = 0; j < thisFinder.rows; j++){
+        thisFinder.grid[i][j].showGrid();
       }
     }
   }
 
   initActions(){
+    const thisFinder = this;
+
     //  STEP ONE
-    if (this.step === 1){
+    if (thisFinder.step === 1){
 
-      this.counter = 0;
-      this.start = undefined;
-      this.end = undefined;
-      this.path = [];
-      this.closedSet = [];
-      this.clicked = [];
-      this.edgeFields = [];
+      thisFinder.counter = 0;
+      thisFinder.start = undefined;
+      thisFinder.end = undefined;
+      thisFinder.path = [];
+      thisFinder.closedSet = [];
+      thisFinder.clicked = [];
+      thisFinder.edgeFields = [];
 
-      this.element.querySelector(select.elements.grid).addEventListener('click', e => {
+      thisFinder.element.querySelector(select.elements.grid).addEventListener('click', e => {
         e.preventDefault();
         if(e.target.classList.contains(classNames.square.square)){
-        this.toggleField(e.target);
+        thisFinder.toggleField(e.target);
         }
       });
 
-      this.element.querySelector(select.elements.button).addEventListener('click', e => {
+      thisFinder.element.querySelector(select.elements.button).addEventListener('click', e => {
         e.preventDefault();
-        this.changeStep(2);
+        thisFinder.changeStep(2);
       });
     }
 
     //  STEP TWO
-    else if (this.step === 2){
+    else if (thisFinder.step === 2){
 
-      this.saveFields();
+      thisFinder.saveFields();
 
-      this.element.querySelector(select.elements.grid).addEventListener('click', e => {
+      thisFinder.element.querySelector(select.elements.grid).addEventListener('click', e => {
         e.preventDefault();
-        this.toggleStart(e.target);
+        thisFinder.toggleStart(e.target);
       });
 
-      this.element.querySelector(select.elements.grid).addEventListener('click', e => {
+      thisFinder.element.querySelector(select.elements.grid).addEventListener('click', e => {
         e.preventDefault();
-        this.toggleFinish(e.target);
+        thisFinder.toggleFinish(e.target);
       });
 
-      this.element.querySelector(select.elements.button).addEventListener('click', e => {
+      thisFinder.element.querySelector(select.elements.button).addEventListener('click', e => {
         e.preventDefault();
-        this.changeStep(3);
+        thisFinder.changeStep(3);
       });
     }
 
     //  STEP THREE
-    else if (this.step === 3){
+    else if (thisFinder.step === 3){
 
-      this.saveFields();
-      this.searchPath();
+      thisFinder.saveFields();
+      thisFinder.searchPath();
 
-      this.element.querySelector(select.elements.button).addEventListener('click', e => {
+      thisFinder.element.querySelector(select.elements.button).addEventListener('click', e => {
         e.preventDefault();
-        this.changeStep(1);
-        this.clearFields();
+        thisFinder.changeStep(1);
+        thisFinder.clearFields();
       });
     }
   }
 
   saveFields(){
-    const squares = this.element.querySelectorAll('#square');
+    const thisFinder = this;
+
+    const squares = thisFinder.element.querySelectorAll('#square');
 
     for (let square of squares){
       const field = {
@@ -127,14 +137,16 @@ class Finder {
         col: square.getAttribute('data-col')
       };
 
-      if (this.grid[field.row][field.col].isClicked === true){
+      if (thisFinder.grid[field.row][field.col].isClicked === true){
         square.classList.add(classNames.square.activeRoad);
       }
     }
   }
 
   clearFields(){
-    const squares = this.element.querySelectorAll('#square');
+    const thisFinder = this;
+
+    const squares = thisFinder.element.querySelectorAll('#square');
 
     for (let square of squares){
       const field = {
@@ -142,174 +154,182 @@ class Finder {
         col: square.getAttribute('data-col')
       };
 
-      if (this.grid[field.row][field.col].isClicked === true){
-        this.grid[field.row][field.col].isClicked = false;
+      if (thisFinder.grid[field.row][field.col].isClicked === true){
+        thisFinder.grid[field.row][field.col].isClicked = false;
       }
-      if (this.grid[field.row][field.col].isStart === true){
-        this.grid[field.row][field.col].isStart = false;
+      if (thisFinder.grid[field.row][field.col].isStart === true){
+        thisFinder.grid[field.row][field.col].isStart = false;
       }
-      if (this.grid[field.row][field.col].isEnd === true){
-        this.grid[field.row][field.col].isEnd = false;
+      if (thisFinder.grid[field.row][field.col].isEnd === true){
+        thisFinder.grid[field.row][field.col].isEnd = false;
       }
-      if (this.grid[field.row][field.col].isWall === false){
-        this.grid[field.row][field.col].isWall = true;
+      if (thisFinder.grid[field.row][field.col].isWall === false){
+        thisFinder.grid[field.row][field.col].isWall = true;
       }
     }
   }
 
   toggleField(fieldElement){
+    const thisFinder = this;
+
     const field = {
       row: fieldElement.getAttribute('data-row'),
       col: fieldElement.getAttribute('data-col')
     };
 
     //  PUSH TO [EDGEFIELDS]
-    if (this.clicked.length > 0){
-      if (this.edgeFields.includes(this.grid[field.row][field.col])){
-        for (let square of this.gridValues) {
-          if (square.neighbors.includes(this.grid[field.row][field.col])){
-            this.edgeFields.push(square);
+    if (thisFinder.clicked.length > 0){
+      if (thisFinder.edgeFields.includes(thisFinder.grid[field.row][field.col])){
+        for (let square of thisFinder.gridValues) {
+          if (square.neighbors.includes(thisFinder.grid[field.row][field.col])){
+            thisFinder.edgeFields.push(square);
           }
         }
 
-        this.grid[field.row][field.col].isClicked = true;
-        this.grid[field.row][field.col].isWall = false;
+        thisFinder.grid[field.row][field.col].isClicked = true;
+        thisFinder.grid[field.row][field.col].isWall = false;
         fieldElement.classList.add(classNames.square.activeRoad);
       }
     }
 
     else {
-      for (let square of this.gridValues){
-        if (square.neighbors.includes(this.grid[field.row][field.col])){
-          this.edgeFields.push(square);
+      for (let square of thisFinder.gridValues){
+        if (square.neighbors.includes(thisFinder.grid[field.row][field.col])){
+          thisFinder.edgeFields.push(square);
         }
       }
-      this.clicked.push(this.grid[field.row][field.col]);
+      thisFinder.clicked.push(thisFinder.grid[field.row][field.col]);
 
-      this.grid[field.row][field.col].isClicked = true;
-      this.grid[field.row][field.col].isWall = false;
+      thisFinder.grid[field.row][field.col].isClicked = true;
+      thisFinder.grid[field.row][field.col].isWall = false;
       fieldElement.classList.add(classNames.square.activeRoad);
     }
   }
 
   toggleStart(fieldElement){
+    const thisFinder = this;
+
     const field = {
       row: fieldElement.getAttribute('data-row'),
       col: fieldElement.getAttribute('data-col')
     };
 
-    if (this.grid[field.row][field.col].isStart === true
+    if (thisFinder.grid[field.row][field.col].isStart === true
         &&
       fieldElement.classList.contains(classNames.square.start)
         &&
-      this.counter === 1)
+      thisFinder.counter === 1)
     {
-      this.grid[field.row][field.col].isStart = false;
+      thisFinder.grid[field.row][field.col].isStart = false;
       fieldElement.classList.remove(classNames.square.start);
       fieldElement.classList.add(classNames.square.activeRoad);
-      this.counter--;
+      thisFinder.counter--;
 
       //  REMOVE STARTING POINT
-      this.start = undefined;
-      this.openSet = [];
+      thisFinder.start = undefined;
+      thisFinder.openSet = [];
     }
     else
-    if (this.grid[field.row][field.col].isClicked === true
+    if (thisFinder.grid[field.row][field.col].isClicked === true
         &&
       fieldElement.classList.contains(classNames.square.activeRoad)
         &&
-        this.counter === 0)
+        thisFinder.counter === 0)
     {
       fieldElement.classList.remove(classNames.square.activeRoad);
-      this.grid[field.row][field.col].isStart = true;
+      thisFinder.grid[field.row][field.col].isStart = true;
       fieldElement.classList.add(classNames.square.start);
-      this.counter++;
+      thisFinder.counter++;
 
       //  SET STARTING POINT
-      this.start = this.grid[field.row][field.col];
-      this.openSet.push(this.start);
+      thisFinder.start = thisFinder.grid[field.row][field.col];
+      thisFinder.openSet.push(thisFinder.start);
     }
   }
 
   toggleFinish(fieldElement){
+    const thisFinder = this;
+
     const field = {
       row: fieldElement.getAttribute('data-row'),
       col: fieldElement.getAttribute('data-col')
     };
 
-    if (this.grid[field.row][field.col].isEnd === true
+    if (thisFinder.grid[field.row][field.col].isEnd === true
         &&
       fieldElement.classList.contains(classNames.square.end)
         &&
-      this.counter === 2)
+      thisFinder.counter === 2)
     {
-      this.grid[field.row][field.col].isEnd = false;
+      thisFinder.grid[field.row][field.col].isEnd = false;
       fieldElement.classList.remove(classNames.square.end);
       fieldElement.classList.add(classNames.square.activeRoad);
-      this.counter--;
+      thisFinder.counter--;
 
       //  REMOVE END POINT
-      this.end = undefined;
+      thisFinder.end = undefined;
     }
     else
-    if (this.grid[field.row][field.col].isClicked === true
+    if (thisFinder.grid[field.row][field.col].isClicked === true
         &&
       fieldElement.classList.contains(classNames.square.activeRoad)
         &&
-      this.counter === 1)
+      thisFinder.counter === 1)
     {
       fieldElement.classList.remove(classNames.square.activeRoad);
-      this.grid[field.row][field.col].isEnd = true;
+      thisFinder.grid[field.row][field.col].isEnd = true;
       fieldElement.classList.add(classNames.square.end);
-      this.counter++;
+      thisFinder.counter++;
 
       //  SET END POINT
-      this.end = this.grid[field.row][field.col];
+      thisFinder.end = thisFinder.grid[field.row][field.col];
     }
   }
 
   searchPath(){
-    while (this.openSet.length > 0){
+    const thisFinder = this;
+
+    while (thisFinder.openSet.length > 0){
       let lowestIndex = 0;
-      for (let i = 0; i < this.openSet.length; i++){
-        if (this.openSet[i].f < this.openSet[lowestIndex].f){
+      for (let i = 0; i < thisFinder.openSet.length; i++){
+        if (thisFinder.openSet[i].f < thisFinder.openSet[lowestIndex].f){
           lowestIndex = i;
         }
       }
-      let current = this.openSet[lowestIndex];
+      let current = thisFinder.openSet[lowestIndex];
 
       //  FINDING PATH
-      if (current === this.end){
+      if (current === thisFinder.end){
         let temp = current;
-        this.path.push(temp);
+        thisFinder.path.push(temp);
         while (temp.previous){
-          this.path.push(temp.previous);
+          thisFinder.path.push(temp.previous);
           temp = temp.previous;
         }
         console.log('DONE!');
       }
 
-      this.removeFromArray(this.openSet, current);
-      this.closedSet.push(current);
+      thisFinder.removeFromArray(thisFinder.openSet, current);
+      thisFinder.closedSet.push(current);
 
       let neighbors = current.neighbors;
 
       for (let i = 0; i < neighbors.length; i++){
         let neighbor = neighbors[i];
 
-        if (!this.closedSet.includes(neighbor) && !neighbor.isWall){
+        if (!thisFinder.closedSet.includes(neighbor) && !neighbor.isWall){
           let tempG = current.g + 1;
-          if (this.openSet.includes(neighbor)){
+          if (thisFinder.openSet.includes(neighbor)){
             if (tempG < neighbor.g){
               neighbor.g = tempG;
             }
           } else {
             neighbor.g = tempG;
-            this.openSet.push(neighbor);
+            thisFinder.openSet.push(neighbor);
           }
 
           //  CALCULATE DISTANCE
-          neighbor.h = this.heuristic(neighbor, this.end);
+          neighbor.h = thisFinder.heuristic(neighbor, thisFinder.end);
           neighbor.f = neighbor.g + neighbor.h;
           neighbor.previous = current;
         }
@@ -317,8 +337,8 @@ class Finder {
     }
 
     //  DISPLAY RESULTS DOM
-    for (let i = 0; i < this.path.length; i++){
-      const pathNode = this.path[i];
+    for (let i = 0; i < thisFinder.path.length; i++){
+      const pathNode = thisFinder.path[i];
       document.querySelector(`[data-row="${pathNode.i}"][data-col="${pathNode.j}"]`).className = 'square path';
     }
   }
@@ -347,9 +367,11 @@ class Finder {
   }
 
   render(){
+    const thisFinder = this;
+    
     let pageData = null;
 
-    switch (this.step){
+    switch (thisFinder.step){
       case 1:
         pageData = { title: 'DRAW ROUTES', btnText: 'FINISH DRAWING' };
         break;
@@ -362,18 +384,18 @@ class Finder {
     }
 
     const generatedHTML = templates.finderWidget(pageData);
-    this.dom = {};
-    this.dom.wrapper = this.element;
-    this.element.innerHTML = generatedHTML;
-    this.setupGrid();
-    this.initActions();
+    thisFinder.dom = {};
+    thisFinder.dom.wrapper = thisFinder.element;
+    thisFinder.element.innerHTML = generatedHTML;
+    thisFinder.setupGrid();
+    thisFinder.initActions();
 
     console.log('---------------------------------------------------');
-    console.log('path:', this.path);
-    console.log('open:', this.openSet);
-    console.log('closed:', this.closedSet);
-    console.log('start:', this.start);
-    console.log('end:', this.end);
+    console.log('path:', thisFinder.path);
+    console.log('open:', thisFinder.openSet);
+    console.log('closed:', thisFinder.closedSet);
+    console.log('start:', thisFinder.start);
+    console.log('end:', thisFinder.end);
     console.log('---------------------------------------------------');
   }
 }
